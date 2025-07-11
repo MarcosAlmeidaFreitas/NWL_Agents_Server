9d5f9679416c70f28d90e70d76e3bbaa706292b6
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2025-07-11 07:09:00 -0300

    atualizações de rotas

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,14 @@ import {
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomsQuestion } from './http/routes/get-room-question.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: 'http:localhost:5173',
+  origin: 'http://localhost:5173',
 })
 
 app.setSerializerCompiler(serializerCompiler)
@@ -26,8 +29,11 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute);
+app.register(createRoomRoute);
+app.register(getRoomsQuestion);
+app.register(createQuestionRoute);
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log(`Esta é a porta : ${env.PORT}`)
-  console.log('HTTP SERVER RUNNING!')
+  // biome-ignore lint/suspicious/noConsole: <explanation>
+  console.log(`HTTP SERVER RUNNING!!! => NA PORTA: ${env.PORT}`)
 })
